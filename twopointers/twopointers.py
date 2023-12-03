4a1c5744ed5ccfb666aff444c63b7c12e74d0d15
nagarjuna gowtham karuturi <gowthamkaruturi@gmail.com>
2023-12-02 21:29:45 -0600

    changes to two pointers code

--- twopointers/twopointers.py
+++ twopointers/twopointers.py
@@ -46,6 +46,17 @@ class Solution(object):
                   res.add(tuple(sorted([p[i],p[j],target])))            
       return res
 
+  def maxArea(self, height: List[int]) -> int:
+    maxArea =0
+    left, right =0,len(height)-1
+    while(left<right):
+        maxArea = max(maxArea, min(height[left],height[right])*(right-left))
+        if height[left]< height[right]:
+            left += 1
+        else:
+            right -= 1
+    return maxArea
+  
 if __name__ == "__main__":
   sol = Solution()
   sol.isPallindrome("A man, a plan, a canal: Panama")
