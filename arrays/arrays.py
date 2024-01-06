2e27d699870bf1c793ebe13c6abdb447c73ff46a
gowtham karuturi <gowthamkaruturi@gmail.com>
2024-01-06 11:31:50 -0600

    channges to arrays.py

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -1,4 +1,4 @@
-from collections import defaultdict,Counter
+from collections import defaultdict,Counter,OrderedDict
 from typing import List
 class TwoPointers(object):
   def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -39,9 +39,11 @@ class TwoPointers(object):
     result = [0]*n
     for i in range(n):
       result[i]= prefixProduct 
+      print(result[i])
       prefixProduct *= nums[i]
     for i in range(n-1,-1,-1):
       result[i] *= suffixProduct
+      print(result[i])
       suffixProduct *= nums[i] 
     return result  
     
@@ -78,9 +80,109 @@ class TwoPointers(object):
               maxCount = max(maxCount, tmp)
           
       return maxCount 
+  
+  def merge_lists(self, lst1:List, lst2:List) -> List:
+    result = []
+    i,j=0,0
+    while i<=len(lst1)-1 and j<=len(lst2)-1:
+       if lst1[i] < lst2[j]:
+        result.append(lst1[i])
+        i +=1 
+       else:
+        result.append(lst2[j])
+        j +=1
+    
+    while i <=len(lst1)-1:
+        result.append(lst1[i])
+        i +=1
+    while j <=len(lst2)-1:
+        result.append(lst2[j])
+        j +=1
+    return result
           
+  def sumOfTwoNumbers(self , lst1:list, k:int) ->list:
+    seen = {}
+    for i in range(len(lst1)):
+       diff = k -lst1[i]
+       if diff in seen:
+          return [i,seen[diff]]
+       else:
+          seen[lst1[i]]=i
+    return []
+       
+  def sumOfTwoNumbersIndices(self , lst1:list, k:int) ->list:
+     lst1.sort()
+     left,right =0,0
+     found = False
+     result =[]
+     while left < right:
+        sum = lst1[right]+lst1[left]
+        if sum > k:
+           left +=1
+        elif sum < k :
+           right +=1
+        else:
+           result.append(lst1[right],lst1[right])
+           return result
+           
+     return result
+           
+           
+  def firstNonRepeating(self, arr):
+      
+      numberDict = {}
+
+      for element in arr:
+          numberDict[element] = numberDict.get(element,0)+1
+      
+      for m in numberDict:
+          if numberDict[m]==1:
+            return m
+      return -1
+
+  def firstNonRepeatingVersion2(self, arr):
+    numberDict = OrderedDict()
+
+    for element in arr:
+        numberDict[element] = numberDict.get(element,0)+1
+      
+    for m in numberDict:
+        if numberDict[m]==1:
+          return m
+    return -1
+  
+  def findTheSecondLargestelement(self, lst :list) -> int:
+    if len(lst)<2:
+       return list[0]
+    maximum = max(lst[0],lst[1])
+    second_maximum = min(lst[0],lst[1])
+    for i in range(2, len(lst)):
+       if lst[i]> maximum:
+        second_maximum = maximum
+        maximum = lst[i]
+       elif lst[i]>second_maximum:
+          second_maximum = lst[i]
+    return second_maximum
+    
+  def findTheNLargestelement(self, lst:list,N:int)-> int:
+     if len(lst)<2:
+       return list[0]
+    
+     while N>1:
+         max = lst.remove(max(lst))
+         N -=1
+     return max(lst)
+
+     
+      
+
 if __name__ == "__main__":
   twoPointers = TwoPointers()
-  print(twoPointers.twoSum([2,7,11,15],9))
-  print(twoPointers.productExceptItself([1,2,3,4]))
+  #print(twoPointers.twoSum([2,7,11,15],9))
+  # print(twoPointers.productExceptItself([1,2,3,4,5]))
+  #print(twoPointers.merge_lists([1,3,4,5],[2,6,7,8] ))
+  #print(twoPointers.sumOfTwoNumbers([1,2,3,4],5))
+  #print(twoPointers.firstNonRepeating([4, 5, 1, 2, 0, 4]))
+  print(twoPointers.findTheSecondLargestelement([4, 5, 1, 2, 0, 4]))
+  print(twoPointers.findTheNLargestelementV2([4, 5, 1, 2, 0, 4]))
       
